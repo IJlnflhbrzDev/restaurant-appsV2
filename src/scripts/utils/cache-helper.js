fd743df82224e2faef98268b6341bc9190facda3
unknown <ijlnflhbrzdev@gmail.com>
2021-05-19 20:10:44 +0700

    Meembuat sedikit fungsi dari setiap kondisi2 cache-helper

--- src/scripts/utils/cache-helper.js
+++ src/scripts/utils/cache-helper.js
@@ -14,10 +14,34 @@ const CacheHelper = {
       .map((filteredName) => caches.delete(filteredName));
   },
 
-  async revalidateCache(requests) {},
+  async revalidateCache(request) {
+    const response = await caches.match(request);
+
+    if (response) {
+      this._fetchRequest(request);
+      return response;
+    }
+    return this._fetchRequest(request);
+  },
 
   async _openCache() {
     return caches.open('Restaurant Apps PWA');
+  },
+
+  async _fetchRequest(request) {
+    const response = await fetch(request);
+
+    if (!response || response.status !== 200) {
+      return response;
+    }
+
+    await this._addCache(request);
+    return response;
+  },
+
+  async _addCache(request) {
+    const cache = await this._openCache();
+    cache.add(request);
   },
 };
 
