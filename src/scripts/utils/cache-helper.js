4c238606791a59cab8f6621b35a6d90e6cbb8da6
unknown <ijlnflhbrzdev@gmail.com>
2021-05-19 19:52:23 +0700

    Membuat 3 kondisi cache

--- src/scripts/utils/cache-helper.js
+++ src/scripts/utils/cache-helper.js
@@ -1,3 +1,11 @@
-const CacheHelper = {};
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-empty-function */
+const CacheHelper = {
+  async cachingAppShell(requests) {},
+
+  async deleteOldCache() {},
+
+  async revalidateCache(requests) {},
+};
 
 export default CacheHelper;
