6bf6c27aa1a143838350049bec845621691238f0
unknown <ijlnflhbrzdev@gmail.com>
2021-05-19 19:57:24 +0700

    Menambahkan Fungsi Cache yaitu _openCache yg akan membuka cache dari web / dari cache yg di ingin kan

--- src/scripts/utils/cache-helper.js
+++ src/scripts/utils/cache-helper.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-empty-function */
 const CacheHelper = {
@@ -6,6 +7,10 @@ const CacheHelper = {
   async deleteOldCache() {},
 
   async revalidateCache(requests) {},
+
+  async _openCache() {
+    return caches.open('Restaurant Apps PWA');
+  },
 };
 
 export default CacheHelper;
