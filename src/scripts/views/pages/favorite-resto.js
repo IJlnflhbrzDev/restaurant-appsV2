e2f3239085c5aa6b54cfb7c5f65b30cd0a49b8a8
unknown <ijlnflhbrzdev@gmail.com>
2021-05-20 20:33:04 +0700

    Melakukan Render pada  Halaman favorite ketika user click tombol like maka akan muncul di route favorite

--- src/scripts/views/pages/favorite-resto.js
+++ src/scripts/views/pages/favorite-resto.js
@@ -1,15 +1,26 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 
+import FavoriteRestaurantIdb from '../../data/database';
+import { restoTemplate } from '../templates/template-html';
+
 const favoriteResto = {
   async render() {
     return `
-      <h2>FAVORITE Restoran Metode Get request API</h2>
+      <div class="content">
+           <h2 class="content__heading">You like Favorite Resto </h2>
+           <div id="resto" class="resto">
+           </div>
+     </div>
     `;
   },
 
   async afterRender() {
-    // Fungsi ini akan dipanggil setelah render()
+    const restaurants = await FavoriteRestaurantIdb.getAllResto();
+    const restaurantContainer = document.querySelector('#resto');
+    restaurants.forEach((restaurant) => {
+      restaurantContainer.innerHTML += restoTemplate(restaurant);
+    });
   },
 };
 
